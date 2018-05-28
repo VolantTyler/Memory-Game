f1305cc0b5d18157c0bdac6ca2138156bac43ca8
Tyler Stahl <tyler@volantweb.com>
2018-05-28 13:43:27 -0400

    declare myTimeout function separately

--- js/app.js
+++ js/app.js
@@ -29,13 +29,13 @@ function shuffle(array) {
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - display the card's symbol (put this functionality in another function that you call from this one) [X]
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) []
  *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) []
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) []
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one) [X]
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one) []
  */
 
 
@@ -48,6 +48,7 @@ let grid = $('ul.deck'); //const?
 let movesCount = 0;
 let currentCard;
 let cardSymbol;
+let anyOpenCard;
 
 function movesPlusOne () {
     let movesDisplay = $('span.moves');
@@ -56,15 +57,16 @@ function movesPlusOne () {
     //TODO: divide movesCount by 2, count each click as move or each pair
 }
 
+function myTimeout() {
+    anyOpenCard.removeClass('open show');
+    openCards = []
+    };
+
 function flipCard() {
     if (!(currentCard.hasClass('open'))) {
         currentCard.addClass('open show');
         openCards.push(cardSymbol);
-        function myTimeout() {
-            anyOpenCard.removeClass('open show');
-            openCards = []
-            };
-        let anyOpenCard = $('li.open');
+        anyOpenCard = $('li.open');
 
         if (openCards.length === 2) {
             if (openCards[0] === openCards[1]) {
@@ -74,7 +76,6 @@ function flipCard() {
                 anyOpenCard.removeClass('open');
                 //erase array
                 openCards = []; 
-                console.log(matchedCards);
               if (matchedCards.length === 16) {
                 alert('You Win! Score = '+movesCount+'');
               } 
